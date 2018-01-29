07e7fcb5e7a7dd0df47be32eb9afe4da810d50bc
Brandon Brown <brandon@Brandons-MacBook-Pro.local>
2018-01-28 21:33:01 -0800

    fixed myapp recipe error

--- client/recipe/recipe.js
+++ client/recipe/recipe.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.recipe', ['ngRoute', 'underscore'])
+angular.module('myApp.recipe', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/account/:userId/recipes', {
