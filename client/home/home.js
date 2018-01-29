479751c15306521e152c3a0c530f5cf80718b37e
Brandon Brown <brandon@Brandons-MacBook-Pro.local>
2018-01-28 21:51:08 -0800

    trying to fix underscore issue still

--- client/home/home.js
+++ client/home/home.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.home', ['ngRoute', 'underscore'])
+angular.module('myApp.home', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when("/", {
