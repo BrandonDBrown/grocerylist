c6672128abd37db9600671737314f730ccb888c4
Brandon Brown <brandon@Brandons-MacBook-Pro.local>
2018-01-28 21:23:45 -0800

    fixed 404 for createlist

--- server/boot/root.js
+++ server/boot/root.js
@@ -9,8 +9,19 @@ module.exports = function(app) {
   var mysqldb = app.dataSources.mysqldb;
 
   // first autoupdate the `Author` model to avoid foreign key constraint failure
-  mysqldb.automigrate('list', function(err) {
-    if (err) throw err;
-    console.log('\nAutoupdated table `list`.');
-  });
+  // mysqldb.automigrate('list', function(err) {
+  //   if (err) throw err;
+  //   console.log('\nAutoupdated table `list`.');
+  // });
 };
+
+
+// "mysqldb": {
+//   "host": "us-cdbr-iron-east-05.cleardb.net",
+//   "port": 3306,
+//   "database": "heroku_eb183e2b7bb2629",
+//   "password": "54c4a575",
+//   "name": "mysqldb",
+//   "user": "b6b0fece3608ee",
+//   "connector": "mysql"
+// }
